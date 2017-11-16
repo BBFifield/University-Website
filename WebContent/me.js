87d0a3bf874c2fb82541e84b3059e5e2b219bd1a
Brandon Fifield <bbf522@mun.ca>
2017-11-16 13:28:58 -0330

    Deleted everything

--- WebContent/me.js
+++
@@ -1,8 +0,0 @@
-/**
- * http://usejsdoc.org/
- */
-var http = require("http");
-var server = http.createServer(function(request,response){
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.end("Helltfkuore");
-}).listen(3337);
